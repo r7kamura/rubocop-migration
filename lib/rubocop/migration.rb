64910246110bfbfb2b67a34bf01c77137ae7d06a
Ryo Nakamura <r7kamura@gmail.com>
2022-10-17 10:03:54 +0900

    Init files by running: bundle gem rubocop-migration

---
+++ lib/rubocop/migration.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "migration/version"
+
+module Rubocop
+  module Migration
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/migration/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Rubocop
+  module Migration
+    VERSION = "0.1.0"
+  end
+end
